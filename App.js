2802d46df37cf7742e6e138ca24b15d95c82a92b
clarklindev <clarklindev@gmail.com>
2025-06-11 16:55:49 +0800

    Adding an Icon Button to a Header

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, Button } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -43,7 +43,15 @@ export default function App() {
             //   };
             // }}
           />
-          <Stack.Screen name="MealDetail" component={MealDetailScreen} />
+          <Stack.Screen
+            name="MealDetail"
+            component={MealDetailScreen}
+            // options={{
+            //   headerRight: () => {
+            //     return <Button title="Button" />;
+            //   },
+            // }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- components/MealDetails.js
+++ components/MealDetails.js
@@ -13,9 +13,7 @@ function MealDetails({
       <Text style={[styles.detailItem, textStyle]}>
         {complexity.toUpperCase()}
       </Text>
-      <Text style={[styles.detailItem, textStyle]}>
-        {affordability.toUpperCase()}
-      </Text>
+      <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
     </View>
   );
 }
